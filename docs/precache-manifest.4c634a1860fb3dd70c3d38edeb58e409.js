ab044414674023bcb81e9d859d22b626ec80df99
Jason <jason.limon@outlook.com>
2020-08-31 23:19:03 -0500

    Docs agregado

---
+++ docs/precache-manifest.4c634a1860fb3dd70c3d38edeb58e409.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9632b0f98358088ea575736309e9ffcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02d0c1d174e407c25d02",
+    "url": "/static/css/main.fa811a40.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02d0c1d174e407c25d02",
+    "url": "/static/js/main.a822240e.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
